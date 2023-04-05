615696a54dc84a292907f59410c660d8a13b6617
Xavier <31611776+airvexx@users.noreply.github.com>
2023-04-05 00:26:57 -0700

    Initial commit

---
+++ Chapter Work/Chapter 2/simple_messages.py
@@ -0,0 +1,8 @@
+## Assign a message to a cariable, and print that message. 
+## Then change the value of the variable to a new message, and print the new message.
+
+greetings = "Hello, Xavier!"
+print(greetings)
+
+greetings = "Goodbye, Xavier!"
+print(greetings)
---
+++ README.md
@@ -0,0 +1,2 @@
+# PCC_v3
+ chapter work for Python Crash Course 3rd edition
