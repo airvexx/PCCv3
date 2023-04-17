5280b50204739860f8cb8d9999143b104e63fa13
Xavier <31611776+airvexx@users.noreply.github.com>
2023-04-17 10:45:57 -0700

    Create 2-4_name_cases.py

---
+++ Chapter Work/Chapter 2/2-4_name_cases.py
@@ -0,0 +1,8 @@
+## Use a variable to represent a person's name, and print that person's name in lowercase, uppercase, and title case.
+
+first_name = "xavier"
+last_name = "sherwood"
+full_name = f"{first_name} {last_name}"
+print(full_name.lower())
+print(full_name.upper())
+print(full_name.title())
