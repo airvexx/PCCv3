b474a789eaed3d8d2f7210521cda260510a66289
Xavier <31611776+airvexx@users.noreply.github.com>
2023-04-17 10:30:49 -0700

    Update 2-3_personal_message.py

--- Chapter Work/Chapter 2/2-3_personal_message.py
+++ Chapter Work/Chapter 2/2-3_personal_message.py
@@ -1,5 +1,5 @@
 ## Use a variable to represent a a person's name, and print a message to that person.
 
 name = "Xavier"
-message = f"Hello, {name.title()}, would you like to learn Python today?"
+message = f"Hello {name.title()}, would you like to learn Python today?"
 print(message)
