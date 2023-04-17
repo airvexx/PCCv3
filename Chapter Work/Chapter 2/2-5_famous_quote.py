5f340ca7b93597b0cfa4c18b7fe282a25807b3ba
Xavier <31611776+airvexx@users.noreply.github.com>
2023-04-17 10:53:29 -0700

    Create 2-5_famous_quote.py

---
+++ Chapter Work/Chapter 2/2-5_famous_quote.py
@@ -0,0 +1,5 @@
+## Find a quote from a famous person you admire. Output should look like: 
+## Albert Einstein once said, "A person who never made a mistake never tried anything new."
+
+print('Robert Anton Wilson once said "In conclusion, there is no conclusion.')
+print('Things will go on as they always have, getting weirder all the time."')
